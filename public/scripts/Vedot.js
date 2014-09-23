40d0c578af502d0036c4ff30f8fea9c0eab7e2de
miikka <datahanska@gmail.com>
2014-09-23 17:17:37 +0300

    Näyttää vedot sekä sen tiedoista osan

--- public/scripts/Vedot.js
+++ public/scripts/Vedot.js
@@ -1,6 +1,46 @@
 var Vedot = function() {
   var self = this;
   self.kategoria = ko.observable({nimi: 'Kaikki', url: '/vedot'});
+
+  self.vedot = ko.observableArray([{
+    booker: 'Veikkaus',
+    pelimuoto: 'Pitkäveto',
+    panos: 2,
+    kerroin: 10.87,
+    voitto: 0,
+    kohteet: [{
+      liiga: 'NFL',
+      ottelu: 'Broncos - Seattle',
+      tyyppi: '12',
+      kerroin: 2.4,
+      veikkaus: '1',
+      tulos: '19-21',
+      osuma: false
+    }]
+  },
+  {
+    booker: 'Nordicbet',
+    pelimuoto: 'Pitkäveto',
+    panos: 3,
+    kerroin: 3.3,
+    voitto: 9.9,
+    kohteet: [{
+      liiga: 'SM-Liiga',
+      ottelu: 'HIFK-Blues',
+      tyyppi: '1x2',
+      kerroin: 3.3,
+      veikkaus: '2',
+      tulos: '1-4',
+      osuma: true
+    }]
+  }]);
+
+  self.valittuVeto = ko.observable();
+  self.valitseVeto = function(veto) {
+    self.valittuVeto(veto);
+  };
+
+  // Kun kategoriaa vaihdetaan
   shouter.subscribe(function(newValue) {
     // Jos tyhjä näytetään kaikki
     if(!newValue.url) {
