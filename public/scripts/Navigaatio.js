4380b0390fb45e0604afa74cbb55959df84cb56c
miikka <datahanska@gmail.com>
2014-09-23 14:40:34 +0300

    Linkit javascriptistä

--- public/scripts/Navigaatio.js
+++ public/scripts/Navigaatio.js
@@ -1,4 +1,20 @@
 var Navigaatio = function() {
   var self = this;
+  // Sivupalkin otsikko
   self.nimi = ko.observable('BetBuddy');
+  // Sivupalkin linkit
+  self.kategoriat = [{
+    nimi: 'Kaikki',
+    url: '/vedot'
+  },
+  {
+    nimi: 'Pitkäveto',
+    url: '/vedot/pelimuoto/pitkäveto'
+  }, {
+    nimi: 'Tulosveto',
+    url: '/vedot/pelimuoto/tulosveto'
+  }, {
+    nimi: 'Moniveto',
+    url: '/vedot/pelimuoto/moniveto'
+  }];
 };
